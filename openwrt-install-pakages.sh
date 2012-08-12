6d74d514775c0d45342518df78e098de211964ee
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2012-08-12 21:44:37 +0300

    Add script to install/configure further packages.
    This should be run after basic extroot system is brought up.

---
+++ openwrt-install-pakages.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+if [ "$1" == "" ]; then
+    echo "Usage: $0 <config file>"
+    exit 1
+fi
+
+set -x
+
+. $1
+
+CRED="$ROUTER_USER@$ROUTER_IP"
+SSH="ssh $SSH_OPTS $CRED"
+
+set -e
+$SSH <<EOF
+set -xe
+export PATH=/bin:/sbin:/usr/bin:/usr/sbin
+opkg update
+
+# USB CDC ACM communication driver, used for modems and other stuff (e.g. TI Launchpad)
+opkg install kmod-usb-acm
+
+# Normal less with '/' (search regexp) command, need to overwrite
+# busybox' version
+opkg install --force-overwrite less
+# Something may crash or misbehave, alwyas have debugging tools
+opkg install gdb strace
+# The language. If readline package is not installed, "import readline" will segfault
+opkg install python readline
+
+# Bluez, etc. require DBus
+opkg install dbus dbus-utils dbus-python
+/etc/init.d/dbus enable
+/etc/init.d/dbus start
+
+# It's all about connectivity - Bluetooth
+opkg install kmod-bluetooth bluez-libs bluez-utils bluez-hcidump python-bluez
+/etc/init.d/bluez-utils enable
+/etc/init.d/bluez-utils start
+
+# The above is enough to run remote-version0.1.py client for PS3 BT BD Remote
+# (will handle adhoc "pairing" on its own).
+
+EOF
